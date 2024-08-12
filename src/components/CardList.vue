5188f69130807120fe74b9fb48c2522031d9dfca
Trisemya <trisemya@gmail.com>
2024-08-12 11:06:45 +0300

    init

---
+++ src/components/CardList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list">
+    <Card v-for="card in cards" :key="card" :cardNum="card" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Card from './Card.vue'
+const {cards, perPage, currentPage} = defineProps<{
+  perPage: number,
+  currentPage: number,
+  cards: number[]
+}>()
+</script>
+
+<style scoped>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
